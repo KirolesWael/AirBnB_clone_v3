c84203800f31c9c63e1d5415ede3fa2d039a9c76
MrBunnyman <kyrollos.beshay02@eng-st.cu.edu.eg>
2024-03-30 18:52:23 +0200

    f

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,15 @@
 #/usr/bin/python3
 
-
+import os
 from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views 
 from models import storage
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
